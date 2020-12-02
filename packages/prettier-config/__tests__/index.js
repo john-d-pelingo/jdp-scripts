db6be7f9b3ced1b76723d05c7a241a09f5818707
John Darryl <johndarrylpelingo@gmail.com>
2020-12-02 17:57:13 +0100

    chore: add more test

--- packages/prettier-config/__tests__/index.js
+++ packages/prettier-config/__tests__/index.js
@@ -2,5 +2,9 @@ const test = require('tape')
 
 test('entry point parses', (t) => {
   t.doesNotThrow(() => require('..'), 'index does not throw')
+  t.doesNotThrow(
+    () => require('../.prettierrc.json'),
+    '.prettierrc.json does not throw',
+  )
   t.end()
 })
