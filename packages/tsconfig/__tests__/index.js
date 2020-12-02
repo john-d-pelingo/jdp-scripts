d5836e0ea6c9d0c1979df1fc12a7b91d46ed8372
John Darryl <johndarrylpelingo@gmail.com>
2020-12-02 17:55:29 +0100

    chore: use index.js as main point

--- packages/tsconfig/__tests__/index.js
+++ packages/tsconfig/__tests__/index.js
@@ -1,14 +1,18 @@
 const test = require('tape')
 
 test('entry point parses', (t) => {
-  t.doesNotThrow(() => require('../tsconfig.base.json'), 'index does not throw')
+  t.doesNotThrow(() => require('..'), 'index does not throw')
+  t.doesNotThrow(
+    () => require('../tsconfig.base.json'),
+    'tsconfig.base.json does not throw',
+  )
   t.doesNotThrow(
     () => require('../tsconfig.react.json'),
-    'index does not throw',
+    'tsconfig.react.json does not throw',
   )
   t.doesNotThrow(
     () => require('../tsconfig.react.dev.json'),
-    'index does not throw',
+    'tsconfig.react.dev.json does not throw',
   )
   t.end()
 })
