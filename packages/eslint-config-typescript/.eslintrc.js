cebe1508dee21086e635e7b672694178796c8876
John Darryl <johndarrylpelingo@gmail.com>
2020-12-02 01:23:54 +0100

    feat: do not extend eslint-config

--- packages/eslint-config-typescript/.eslintrc.js
+++ packages/eslint-config-typescript/.eslintrc.js
@@ -7,15 +7,36 @@ const config = {
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint', 'import', 'jsx-a11y'],
   extends: [
-    '@jdp-dev/eslint-config',
+    'eslint:recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
+    'prettier',
     'prettier/@typescript-eslint',
     'plugin:import/typescript',
   ],
   rules: {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/no-var-requires': 'off',
+    'import/no-default-export': 'error',
+    'import/no-deprecated': 'warn',
+    'import/order': [
+      'error',
+      {
+        pathGroupsExcludedImportTypes: ['builtin'],
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          'unknown',
+          'parent',
+          'sibling',
+          'index',
+        ],
+        'newlines-between': 'always',
+        alphabetize: { order: 'asc' },
+      },
+    ],
+    'import/prefer-default-export': 'off',
   },
   settings: {
     'import/ignore': ['node_modules'],
